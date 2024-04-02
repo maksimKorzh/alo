6fc0d713b40806efc1c20edfd44bb8386d85a660
Code Monkey King <freesoft.for.people@gmail.com>
2024-04-02 18:03:26 +0300

    Update light-organ.ino

--- light-organ.ino
+++ light-organ.ino
@@ -1,33 +1,41 @@
-int pins[] = {13, 12, 10, 9, 8, 7, 6, 5};
+/*********************************\
+
+   Arduino Light Organ (backend)
+
+\*********************************/
+
+int pins[] = {10, 9, 8, 7, 6, 5, 4, 3, 2};
 char pin;
 
 void setup() {
-  // initialize serial:
   Serial.begin(115200);
-
   for (int i = 0; i < 8; i++) pinMode(pins[i], OUTPUT);
+  // Reset LPT strobe pin
+  digitalWrite(5, LOW);
+  digitalWrite(5, HIGH);
 }
 
 void loop() {
   while (Serial.available() > 0) {
-	  pin = Serial.read();
+    pin = Serial.read();
     switch (pin) {
-      case 'a': digitalWrite(13, HIGH); break;
-      case 'b': digitalWrite(12, HIGH); break;
-      case 'c': digitalWrite(11, HIGH); break;
-      case 'd': digitalWrite(10, HIGH); break;
-      case 'e': digitalWrite(9, HIGH); break;
-      case 'f': digitalWrite(8, HIGH); break;
-      case 'g': digitalWrite(7, HIGH); break;
-      case 'h': digitalWrite(6, HIGH); break;
-      case 'A': digitalWrite(13, LOW); break;
-      case 'B': digitalWrite(12, LOW); break;
-      case 'C': digitalWrite(11, LOW); break;
-      case 'D': digitalWrite(10, LOW); break;
-      case 'E': digitalWrite(9, LOW); break;
-      case 'F': digitalWrite(8, LOW); break;
-      case 'G': digitalWrite(7, LOW); break;
-      case 'H': digitalWrite(6, LOW); break;
+      // Data
+      case 'A': digitalWrite(pins[0], HIGH); break;
+      case 'B': digitalWrite(pins[1], HIGH); break;
+      case 'C': digitalWrite(pins[2], HIGH); break;
+      case 'D': digitalWrite(pins[3], HIGH); break;
+      case 'E': digitalWrite(pins[4], HIGH); break;
+      case 'F': digitalWrite(pins[5], HIGH); break;
+      case 'G': digitalWrite(pins[6], HIGH); break;
+      case 'H': digitalWrite(pins[7], HIGH); break;
+      case 'a': digitalWrite(pins[0], LOW); break;
+      case 'b': digitalWrite(pins[1], LOW); break;
+      case 'c': digitalWrite(pins[2], LOW); break;
+      case 'd': digitalWrite(pins[3], LOW); break;
+      case 'e': digitalWrite(pins[4], LOW); break;
+      case 'f': digitalWrite(pins[5], LOW); break;
+      case 'g': digitalWrite(pins[6], LOW); break;
+      case 'h': digitalWrite(pins[7], LOW); break;
     }
   }
 }
